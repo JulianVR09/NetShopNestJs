dc3cfc9dcc18404c51ab3593aeedf1eb6dbccdcd
JulianVR09 <julianvillacis.02@gmail.com>
2025-01-17 15:55:04 -0500

    feat: add Swagger, database connection and CORS support
    - Add @nestjs/swagger for API documentation
    - Configure TypeORM with PostgreSQL connection using @nestjs/config for environment variables
    - Enable CORS support for cross-origin requests

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.setGlobalPrefix('api/v1');
+
+  app.enableCors()  
+
+  const config = new DocumentBuilder()
+  .setTitle('NetShop')
+  .setDescription('The NetShop API documentation')
+  .addTag('net-shop')
+  .setVersion('1.0')
+  .build()
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, documentFactory);
+  
   await app.listen(3000);
 }
 bootstrap();
